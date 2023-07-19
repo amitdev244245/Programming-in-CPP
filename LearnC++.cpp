5341b1f4995e93c547393b7dfa1dde97800865fe
Amit Sharma <amitdev244245@gmail.com>
2023-07-20 04:52:22 +0530

    first commit

---
+++ LearnC++.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+/*----------------------------------------------------------------------------------
+#1. Write a program that takes an input of age and prints if you are adult or not.
+----------------------------------------------------------------------------------*/
+
+// int main()
+// {
+//     int age;
+//     cin >> age;
+//     if (age >= 18)
+//     {
+//         cout << "Adult";
+//     }
+//     else
+//     {
+//         cout << "Not Adult";
+//     }
+//     return 0;
+// }
+
+/*----------------------------------------------------------------------------------
+#2. Write a program that takes marks as input and prints the corresponding grades.
+a. Below 25 - F
+b. 25 to 44 - E
+c. 45 to 49 - D
+d. 50 to 59 - C
+e. 60 to 79 - B
+f. 80 to 100 - A
+----------------------------------------------------------------------------------*/
+
+// int main()
+// {
+//     int marks;
+//     cin >> maeks;
+//     if (marks < 25)
+//     {
+//         cout << "F";
+//     }
+//     else if (marks <= 44)
+//     {
+//         cout << "E";
+//     }
+//     else if (marks <= 49)
+//     {
+//         cout << "D";
+//     }
+//     else if (marks <= 59)
+//     {
+//         cout << "C";
+//     }
+//     else if (marks <= 79)
+//     {
+//         cout << "B";
+//     }
+//     else if (marks <= 100)
+//     {
+//         cout << "A";
+//     }
+//     else
+//     {
+//         cout << "Wrong marks entered!";
+//     }
+//     return 0;
+// }
+
+/*----------------------------------------------------------------------------------
+#3. Write a program that takes an input of age and prints their eligibility for job.
+a. If age < 18,
+   print-> not eligible for job
+b. If age >= 18,
+   print-> eligible for job
+c. If age >= 55 and age<=57,
+   print-> not eligible for job, but retirement soon
+d. If age > 57,
+   print-> retirement time
+----------------------------------------------------------------------------------*/
