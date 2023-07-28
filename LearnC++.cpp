0ffa1c54317dc83c0f465d9f1e3cb7db8b0131fc
Amit Sharma <amitdev244245@gmail.com>
2023-07-29 04:42:38 +0530

    added program #6.1 Rectangular Star Pattern **

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -200,20 +200,20 @@ Output:
 * * * * * *
 ----------------------------------------------------------------------------------*/
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
