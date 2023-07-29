f9458e23b0981efb0e336b04dcbc6899ff909062
Amit Sharma <amitdev244245@gmail.com>
2023-07-29 07:19:48 +0530

    Added #6.4 Program: Right-Angled Number Pyramid - II

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -289,3 +289,40 @@ Result:
 //     }
 //     return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.4 Write a program on Right-Angled Number Pyramid – II
+
+Input Format: N = 3
+Result:
+1
+2 2
+3 3 3
+
+Input Format: N = 6
+Result:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
