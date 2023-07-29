3c8e603034d1a2b91a380fc91b32af5bf77d3772
Amit Sharma <amitdev244245@gmail.com>
2023-07-29 07:51:46 +0530

    Added #6.6 Program: Inverted Numbered Right Pyramid

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -363,3 +363,40 @@ Result:
 //     }
 //     return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.6 Write a program on Inverted Numbered Right Pyramid
+
+Input Format: N = 3
+Result:
+1 2 3
+1 2
+1
+
+Input Format: N = 6
+Result:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+----------------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
