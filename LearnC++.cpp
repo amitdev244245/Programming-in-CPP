d16c518c9b695995c36f14699b41a94981e56b6d
Amit Sharma <amitdev244245@gmail.com>
2023-07-31 05:14:03 +0530

    Updated #6.8 Program: Inverted Star Pyramid

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -466,20 +466,25 @@ Result:
 // #include <iostream>
 // using namespace std;
 
-// int main(){
+// int main()
+// {
 //   int n;
-//   cin>>n;
-//   for(int i=1; i<=n; i++){
-//     for(int j=0; j<i; j++){
-//       cout<<" ";
+//   cin >> n;
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = 0; j < i; j++)
+//     {
+//       cout << " ";
 //     }
-//     for(int j=0; j<=(2*n)-(2*i); j++){
-//       cout<<"*";
+//     for (int j = 0; j < (2 * n) - (2 * i + 1); j++)
+//     {
+//       cout << "*";
 //     }
-//     for(int j=0; j<i; j++){
-//       cout<<" ";
+//     for (int j = 0; j < i; j++)
+//     {
+//       cout << " ";
 //     }
-//     cout<<endl;
+//     cout << endl;
 //   }
 //   return 0;
 // }
