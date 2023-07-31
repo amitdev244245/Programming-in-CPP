6616412943b7f7a81ebf27a8bc52e2062f0e30be
Amit Sharma <amitdev244245@gmail.com>
2023-07-31 06:03:51 +0530

    Added #6.11 Program: Binary Number Triangle Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -600,3 +600,41 @@ Result:
 //   }
 //   return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.11 Write a program on Binary Number Triangle Pattern
+
+Input Format: N = 3
+Result:
+1
+01
+101
+
+Input Format: N = 6
+Result:
+1
+01
+101
+0101
+10101
+010101
+----------------------------------------------------------------------------------*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int start=-1;
+//   for(int i=0; i<n; i++){
+//     if(i%2==0)  start=1;
+//     else start=0;
+//     for(int j=0; j<=i; j++){
+//       cout<<start;
+//       start=1-start;
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }
