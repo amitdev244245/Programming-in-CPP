511bcda211c37118cc69d4cfa449318f3ba6bf9d
Amit Sharma <amitdev244245@gmail.com>
2023-08-01 21:53:37 +0530

    Added #6.12 Program: Number Crown Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -638,3 +638,52 @@ Result:
 //   }
 //   return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.12 Write a program on Number Crown Pattern
+
+Input Format: N = 3
+Result:
+1    1
+12  21
+123321
+
+Input Format: N = 6
+Result:
+1          1
+12        21
+123      321
+1234    4321
+12345  54321
+123456654321
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int spaces = 2 * (n - 1);
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << j;
+//     }
+
+//     for (int j = 1; j <= spaces; j++)
+//     {
+//       cout << " ";
+//     }
+
+//     for (int j = i; j >= 1; j--)
+//     {
+//       cout << j;
+//     }
+//     cout << endl;
+//     spaces -= 2;
+//   }
+//   return 0;
+// }
