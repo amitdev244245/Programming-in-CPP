4c4c73840ead2bd539ac220643a92514add2bf59
Amit Sharma <amitdev244245@gmail.com>
2023-08-02 16:03:46 +0530

    Added #6.13 Program: Increasing Number Triangle Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -687,3 +687,40 @@ Result:
 //   }
 //   return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.13 Write a program on Increasing Number Triangle Pattern
+
+Input Format: N = 3
+Result:
+1
+2 3
+4 5 6
+
+Input Format: N = 6
+Result:
+1
+2  3
+4  5  6
+7  8  9  10
+11  12  13  14  15
+16  17  18  19  20  21
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int num=1;
+//   for(int i=1; i<=n; i++){
+//     for(int j=1; j<=i; j++){
+//       cout<<num<<" ";
+//       num++;
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }
