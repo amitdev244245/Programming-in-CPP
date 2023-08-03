c34a0ae3ba764238446585713c76efed3a2c2502
Amit Sharma <amitdev244245@gmail.com>
2023-08-03 20:28:01 +0530

    Added #6.15 Program: Reverse Letter Triangle Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -761,3 +761,40 @@ A B C D E F
 //     }
 //     return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.15 Write a program on Reverse Letter Triangle Pattern
+
+Input Format: N = 3
+Result:
+A B C
+A B
+A
+
+Input Format: N = 6
+Result:
+A B C D E F
+A B C D E
+A B C D
+A B C
+A B
+A
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (char j = 'A'; j <= 'A' + (n-i-1); j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
