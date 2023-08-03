4cd2ddcc83da1d5ae02a0c5c87b0a9458efcce2f
Amit Sharma <amitdev244245@gmail.com>
2023-08-03 20:31:46 +0530

    Added #6.17 Program: Alpha-Hill Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -836,3 +836,52 @@ F F F F F F
 //     }
 //     return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.17 Write a program on Alpha-Hill Pattern
+
+Input Format: N = 3
+Result:
+  A
+ ABA
+ABCBA
+
+
+Input Format: N = 6
+Result:
+     A
+    ABA
+   ABCBA
+  ABCDCBA
+ ABCDEDCBA
+ABCDEFEDCBA
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         char ch = 'A';
+//         int mid = (i * 2 + 1) / 2;
+
+//         for (int j = 1; j <= 2 * i + 1; j++)
+//         {
+//             cout << ch;
+//             if (j <= mid) ch++;
+//             else ch--;
+//         }
+
+//         cout << endl;
+//     }
+//     return 0;
+// }
