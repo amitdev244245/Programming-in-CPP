eaa408cba49e219ed7bd08125590be4c48f9e5ab
Amit Sharma <amitdev244245@gmail.com>
2023-08-03 17:22:36 +0530

    Added #6.14 Program: Increasing Letter Triangle Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -724,3 +724,22 @@ Result:
 //   }
 //   return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.14 Write a program on Increasing Letter Triangle Pattern
+
+Input Format: N = 3
+Result:
+A
+A B
+A B C
+
+Input Format: N = 6
+Result:
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D E F
+----------------------------------------------------------------------------------*/
