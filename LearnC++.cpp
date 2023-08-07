1afb698bc4c5f382303a143efae72e8fca2574c1
Amit Sharma <amitdev244245@gmail.com>
2023-08-07 21:59:27 +0530

    Added #6.21 Program: Hollow Rectangle Pattern

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -1064,3 +1064,42 @@ Result:
 
 //   return 0;
 // }
+
+/*----------------------------------------------------------------------------------
+#6.21 Write a program on Hollow Rectangle Pattern
+
+Input Format: N = 3
+Result:
+***
+* *
+***
+
+Input Format: N = 6
+Result:
+******
+*    *
+*    *
+*    *
+*    *
+******
+----------------------------------------------------------------------------------*/
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for(int j=1; j<=n; j++){
+//       if(i==1 || j==1 || i==n || j==n) cout<<"*";
+//       else cout<<" ";
+//     }
+//     cout << endl;
+//   }
+
+//   return 0;
+// }
